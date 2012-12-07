d536faf3025756953214eecb90be1e8f40a93bd5
hao <hao@drc-machine.(none)>
2012-12-07 16:27:20 -0500

    first commit with physics simulation working, require SmartGrab

--- hoseexp1_execute.py
+++ hoseexp1_execute.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         RaveSetDebugLevel(DebugLevel.Debug)
         time.sleep(0.25)
 
-        timestep = 0.00045
+        timestep = 0.0005
         with env:
             #load from an xml file
             env.StopSimulation()
@@ -93,7 +93,7 @@ if __name__ == "__main__":
         #we need to change the time step to get fine simulation results
         #ode could crash if the timestep is too big
         env.StopSimulation()
-        env.StartSimulation(0.0005)
+        env.StartSimulation(0.0001)
         CloseLeftHand(robot,pi)
         env.StopSimulation()
         env.StartSimulation(timestep)
